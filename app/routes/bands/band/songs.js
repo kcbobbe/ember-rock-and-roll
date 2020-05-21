43f1076e35f102ceedd83b9cc1c584d9a4909a2d
Katie Bobbe <kcbobbe@gmail.com>
2020-05-21 15:06:50 -0400

    start chapter 8, controllers

--- app/routes/bands/band/songs.js
+++ app/routes/bands/band/songs.js
@@ -1,7 +1,27 @@
 import Route from '@ember/routing/route';
+import Song from 'rarwe/models/song'
 
 export default Route.extend({
     model: function() {
         return this.modelFor('bands.band')
+    },
+
+    actions: {
+        createSong: function(){
+            var controller = this.get('controller');
+            var band = this.modelFor('bands.band');
+            var title = controller.get('title');
+
+            var song = Song.create({ title: title, band: band });
+            band.get('songs').pushObject(song);
+            controller.set('title', '');
+        },
+        updateRating: function(params) {
+            var song = params.item,
+            rating = params.rating;
+
+            song.set('rating', rating)
+
+        }
     }
 })
